98951066a22fbe5fe0c8941fd9a8377c29a3f59b
alan fabricio <alansosa01@yahoo.es>
2018-10-30 17:15:59 -0600

    incorporacion del contenido de las plantillas

--- public/vistas/resultado_busqueda.php
+++ public/vistas/resultado_busqueda.php
@@ -221,7 +221,7 @@ session_start();
   <br>
   <br>
 
-  <!-- Footer -->
+  <!Documento de cierre>
   <div class="navbar navbar-default navbar-fixed-bottom" id="footer">
       <div class="container">
           <div class="row text-center text-xs-center text-sm-left text-md-left ">
@@ -231,26 +231,22 @@ session_start();
 
               <div  class="col-xs-6 col-sm-6 col-md-4" >
                   <a  href="#"><span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> <strong class="opcion coloremail">Descarga la App</strong></a>
+                    <ul class="list-unstyled quick-links">
+                    <div class="principal">
+                    </div>
+                    </ul>
+               </div>
 
-                  <ul class="list-unstyled quick-links">
+              <div  class="col-xs-6 col-sm-6 col-md-4" >
+                  <span  class="glyphicon glyphicon-question-sign" aria-hidden="true"></span> <strong id="coloayuda">Ayuda y Contacto</strong></a>
+                   <ul class="list-unstyled quick-links">
+                    <a  href="#"><span  aria-hidden="true"></span> <strong class="opcion coloremail"><small>aeodanli@gmail.com</small></strong></a>
+                  </ul>
+              </div>
 
-                      <div class="principal"
-                  </div>
-
-                  <!--LIK DE DESCARGA DE LA APP-->
-              </ul>
-          </div>
-          <div  class="col-xs-6 col-sm-6 col-md-4" >
-              <span  class="glyphicon glyphicon-question-sign" aria-hidden="true"></span> <strong id="coloayuda">Ayuda y Contacto</strong></a>
-
-              <ul class="list-unstyled quick-links">
-                  <a  href="#"><span  aria-hidden="true"></span> <strong class="opcion coloremail"><small>aeodanli@gmail.com</small></strong></a>
-
-
-              </ul>
-          </div>
+            </div>
+         </div>
       </div>
-  </div>
-  </div>
-  </body>
+     </body>
   </html>
+  <!fin de documento de cierre>
