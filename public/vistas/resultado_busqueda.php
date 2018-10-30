0d4f07d3ca555ac862a2cad3658d7dda4eb0b97b
alan fabricio <alansosa01@yahoo.es>
2018-10-30 17:11:23 -0600

    incororacion del contenido de las plantillas

--- public/vistas/resultado_busqueda.php
+++ public/vistas/resultado_busqueda.php
@@ -1,59 +1,93 @@
 <?php
     include_once '../../config/ConexionABaseDeDatos.php';
-<<<<<<< HEAD
-    include_once '../plantillas/documento-inicio.inc.php';
-    include_once '../plantillas/barra-de-navegacion-navbar.inc.php';
-=======
->>>>>>> df023f5d8256af222a7c7eb6a198a195d9272be2
+  ?>
+<!Documento de Inicio>
+<html  lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php
+        if (!isset($titulo) || empty($titulo)) {
+            $titulo = 'Agenda Electroníca Oriental';
+        }
+        echo "<title>$titulo</title>";
+        ?>
+        <title>Agenda Electrónica Oriental</title>
+
+        <link rel="shortcut icon" href="../imagenes/aeo.ico" />
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/estilos.css" rel="stylesheet">
+    </head>
+    <body>
+<!Fin de documento de Inicio>
+
+<!inicio de barra de navegacion>
+<?php
+session_start();
 ?>
+<nav class="navbar navbar-default ">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a  href="../index.php"><img class="btn-card" src="../imagenes/aeo.png" align="left" height="50"></a><!--Para ponerle una img ala pagina -->
+            <a class="navbar-brand" href="../index.php"><strong>Agenda Electrónica Oriental</strong></a>
+        </div>
+
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <?php
+                if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
+                    if (isset($_SESSION['rol']) && !empty($_SESSION['rol']) && $_SESSION['rol'] == 2) {
+                        ?>
+                        <li id="boton" class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> <strong>Panel de Control</strong><span class="caret"></span></a>
+                            <ul id="despliege"class="dropdown-menu" role="menu">
+                                <li> <a id = "colorIniciosecion" href = "../Vistas/login.php"><img src="../imagenes/config.png" height="15"></img> <strong>Panel de Control </strong></a></li>
+                                <li><a href="../Vistas/editarUsuarioNormal.php"><img src="../imagenes/administracioncuenta.jpg" height="15"></img> <strong>Edición de Cuenta</strong></a></li>
+                            </ul>
+                        </li>
+                        <li> <a id="colorIniciosecion" href="../cerrarSessionLogin.php"><span class="glyphicon glyphicon-off" aria-hidden="true"></span> <strong>Cerrar Sesión</strong></a></li>
+
+                        <?php
+                    } else {
+                        ?><li id="boton" class="dropdown">
+                               <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> <strong>Panel de Control</strong><span class="caret"></span></a>
+                               <ul id="despliege"class="dropdown-menu" role="menu">
+                                 <li><a href="../Vistas/mostrar_usuarios.php"><img src="../imagenes/administracioncuenta.jpg" height="15"></img> <strong>Administración de Cuenta</strong></a></li>
+                                 <li><a href="../Vistas/administracion-de-perfiles.php"><img src="../imagenes/administracionperfil.jpg" height="15"></img> <strong>Administración de Perfil</strong></a></li>
+                             </ul>
+                        </li>
+                        <li> <a id="colorIniciosecion" href="../cerrarSessionLogin.php"><span class="glyphicon glyphicon-off" aria-hidden="true"></span> <strong>Cerrar Sesión</strong></a></li>
+                        <?php
+                        $message = 'Usuario o Contraseña incorrectas desde la sesion';
+                           }
+                           ?>
+                    <?php
+
+                } else {
+                    ?>
+                    <li> <a id = "colorIniciosecion" href = "../Vistas/login.php"><span class = "glyphicon glyphicon-log-in" aria-hidden = "true"></span> <strong>Iniciar Sesión</strong></a></li>
+                    <li> <a id = "colorIniciosecion" href = "../Vistas/registrarCuentaUsuario.php"><span class = "glyphicon glyphicon-plus" aria-hidden = "true"></span> <strong>Registrarse</strong></a></li>
+                        <?php
+                    }
+                    ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!fin barra de navegacion>
+
 <script
     src="../js/jquery-2.2.4.min.js"
  ></script>
      <link href="../css/estilos_alan.css" rel="stylesheet">
 
-<<<<<<< HEAD
-=======
-     <nav class="navbar navbar-default ">
-         <div class="container">
-             <div class="navbar-header">
-
-                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                     <span class="sr-only"></span>
-                     <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                 </button>
-                 <a  href="../index.php"><img class="btn-card" src="../imagenes/aeo.png" align="left" height="50"></a><!--Para ponerle una img ala pagina -->
-                 <a class="navbar-brand" href="../index.php"><strong>Agenda Electrónica Oriental</strong></a>
-             </div>
-
-             <div id="navbar" class="navbar-collapse collapse">
-                 <ul class="nav navbar-nav navbar-right">
-                     <li> <a id="colorIniciosecion" href="../../config/cerrarSessionLogin.php"><span class="glyphicon glyphicon-off" aria-hidden="true"></span> <strong>Cerrar Sesión</strong></a></li>
-
-                     <li id="boton" class="dropdown">
-                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> <strong>Panel de Control</strong><span class="caret"></span></a>
-                         <ul id="despliege"class="dropdown-menu" role="menu">
-                             <li><a href="../Vistas/mostrar_usuarios.php"><img src="../imagenes/administracioncuenta.jpg" height="15"></img> <strong>Administración de Cuenta</strong></a></li>
-                             <li><a href="../Vistas/administracion-de-perfiles.php"><img src="../imagenes/administracionperfil.jpg" height="15"></img> <strong>Administración de Perfil</strong></a></li>
-                         </ul>
-                     </li>
-                     <!--va iniciar secion o registrarce -->
-                 </ul>
-             </div>
-         </div>
-
-     </nav>
-
-     <?php
-     $busquedas = null;
-     ?>
-     <script
-         src="../js/jquery-2.2.4.min.js"
-     ></script>
-     <link href="../css/estilos_alan.css" rel="stylesheet">
-
->>>>>>> df023f5d8256af222a7c7eb6a198a195d9272be2
 
      <div class="container" style="padding-top: 3px;padding-bottom: 3px; " >
          <div class="form-group" >
@@ -64,12 +98,8 @@
                      <div class="col-md-6 col-xs-6 col-sm-6 inner-addon right-addon">
                        <div class="input-group">
                          <span class="input-group-btn">
-<<<<<<< HEAD
                            <button   type="submit"  style=" color: black; background:#F2F2F2"  class="btn btn-light form-control "><strong><span style="padding: 1px ; width: 2px"
                              class="glyphicon glyphicon-search" aria-hidden="true"></span></strong></button>
-=======
-                           <button id="separador"  type="submit" style=" color: white" class="btn btn-warning form-control "><strong><span class="glyphicon glyphicon-search"></span></strong></button>
->>>>>>> df023f5d8256af222a7c7eb6a198a195d9272be2
                          </span>
                            <input id="busqueda" type="text" class="form-control" name="busqueda"  placeholder="Contacto a buscar"
                            required oninvalid="setCustomValidity('Ingrese la busqueda.')" oninput="setCustomValidity('')">
@@ -108,32 +138,10 @@
      </div>
 
 
-<<<<<<< HEAD
 
 
 
 
-=======
-     <!DOCTYPE html>
-     <html  lang="es">
-         <head>
-             <meta charset="UTF-8">
-             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-             <meta name="viewport" content="width=device-width, initial-scale=1">
-             <?php
-             if (!isset($titulo) || empty($titulo)) {
-                 $titulo = 'Agenda Electroníca Oriental';
-             }
-             echo "<title>$titulo</title>";
-             ?>
-             <title>Agenda Electrónica Oriental</title>
-
-             <link rel="shortcut icon" href="../imagenes/aeo.ico" />
-             <link href="../css/bootstrap.min.css" rel="stylesheet">
-             <link href="../css/estilos.css" rel="stylesheet">
-         </head>
-         <body>
->>>>>>> df023f5d8256af222a7c7eb6a198a195d9272be2
 
  <div id="estilo-contenedor-textocategoria"class="container">
     <div class="row"  id="fila"  >
@@ -245,4 +253,4 @@
   </div>
   </div>
   </body>
-  </html>  
+  </html>
