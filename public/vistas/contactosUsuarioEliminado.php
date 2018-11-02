bc34f63b023ebc609a5050a2350edaac96dd0648
bessyRamos <bessyramos3@gmail.com>
2018-11-02 16:57:50 -0600

    modificacion de las plantillas de inicio, navbar,y cierren

--- public/vistas/contactosUsuarioEliminado.php
+++ public/vistas/contactosUsuarioEliminado.php
@@ -1,10 +1,64 @@
 <?php
 $titulo = 'Contactos Eliminados';
-include_once '../plantillas/documento-inicio.inc.php';
-include_once '../plantillas/barra-de-navegacion-navbar.inc.php';
 if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
     if (isset($_SESSION['rol']) && !empty($_SESSION['rol']) && $_SESSION['rol'] == 2) {
         ?>
+        <!DOCTYPE html>
+        <html  lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <?php
+                if (!isset($titulo) || empty($titulo)) {
+                    $titulo = 'Agenda Electroníca Oriental';
+                }
+                echo "<title>$titulo</title>";
+                ?>
+                <title>Agenda Electrónica Oriental</title>
+
+                <link rel="shortcut icon" href="../imagenes/aeo.ico" />
+                <link href="../css/bootstrap.min.css" rel="stylesheet">
+                <link href="../css/estilos.css" rel="stylesheet">
+            </head>
+            <body>
+        <!fin documento inicio>
+
+        <!Barra de navegacion Navbar>
+        <script type="text/javascript" src="../js/jquery-2.2.4.min.js"></script>
+        <head><link href="../css/estilos_melvin.css" rel="stylesheet"></head>
+        <nav class="navbar navbar-default ">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a  href="../index.php"><img class="btn-card" src="../imagenes/aeo.png" align="left" height="50"></a><!--Para ponerle una img ala pagina -->
+                    <a class="navbar-brand" href="../index.php"><strong>Agenda Electrónica Oriental</strong></a>
+                </div>
+
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li> <a id="colorIniciosecion" href="../../config/cerrarSessionLogin.php"><span class="glyphicon glyphicon-off" aria-hidden="true"></span> <strong>Cerrar Sesión</strong></a></li>
+
+                        <li id="boton" class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> <strong>Panel de Control</strong><span class="caret"></span></a>
+                            <ul id="despliege"class="dropdown-menu" role="menu">
+                                <li><a href="../Vistas/mostrar_usuarios.php"><img src="../imagenes/administracioncuenta.jpg" height="15"></img> <strong>Administración de Cuenta</strong></a></li>
+                                <li><a href="../Vistas/administracion-de-perfiles.php"><img src="../imagenes/administracionperfil.jpg" height="15"></img> <strong>Administración de Perfil</strong></a></li>
+                            </ul>
+                        </li>
+                        <!--va iniciar secion o registrarce -->
+                    </ul>
+                </div>
+            </div>
+
+        </nav>
+        <!Fin de navbar>
 
         <head>
             <link href="../css/estiloslogin.css" rel="stylesheet">
@@ -91,9 +145,48 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
             });
             };
         </script>
-        <?php
-        include_once '../plantillas/documento-cierre.inc.php';
-        ?>
+
+
+        //inicio documento cierre
+          <script src="../js/jquery.min.js"></script>
+          <script src="../js/bootstrap.min.js"></script>
+          <br>
+          <br>
+
+          <!-- Footer -->
+          <div class="navbar navbar-default navbar-fixed-bottom" id="footer">
+              <div class="container">
+                  <div class="row text-center text-xs-center text-sm-left text-md-left ">
+                      <div  class="col-xs-6 col-sm-6 col-md-4" >
+                          <a  href="../Vistas/acercadeweb.php"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> <strong class="opcion coloremail">Acerca de Desarrolladores</strong></a>
+                      </div>
+
+                      <div  class="col-xs-6 col-sm-6 col-md-4" >
+                          <a  href="#"><span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> <strong class="opcion coloremail">Descarga la App</strong></a>
+
+                          <ul class="list-unstyled quick-links">
+
+                              <div class="principal"
+                          </div>
+
+                          <!--LIK DE DESCARGA DE LA APP-->
+                      </ul>
+                  </div>
+                  <div  class="col-xs-6 col-sm-6 col-md-4" >
+                      <span  class="glyphicon glyphicon-question-sign" aria-hidden="true"></span> <strong id="coloayuda">Ayuda y Contacto</strong></a>
+
+                      <ul class="list-unstyled quick-links">
+                          <a  href="#"><span  aria-hidden="true"></span> <strong class="opcion coloremail"><small>aeodanli@gmail.com</small></strong></a>
+
+
+                      </ul>
+                  </div>
+              </div>
+          </div>
+          </div>
+          </body>
+          </html>
+        //fin de documento cierre
         <?php
     } else {
         header('Location: ../webaeo');
@@ -102,5 +195,3 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
     header('Location: ../webaeo');
 }
 ?>
-
-
--- public/vistas/edicionDePerfilUsuarioNormal.php
+++ public/vistas/edicionDePerfilUsuarioNormal.php
@@ -1,10 +1,66 @@
 <?php
 $titulo = "Editar Perfil";
-include_once '../plantillas/documento-inicio.inc.php';
-include_once '../plantillas/barra-de-navegacion-navbar.inc.php';
 if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
     if (isset($_SESSION['rol']) && !empty($_SESSION['rol']) && $_SESSION['rol'] == 2) {
         ?>
+
+        <!DOCTYPE html>
+        <html  lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <?php
+                if (!isset($titulo) || empty($titulo)) {
+                    $titulo = 'Agenda Electroníca Oriental';
+                }
+                echo "<title>$titulo</title>";
+                ?>
+                <title>Agenda Electrónica Oriental</title>
+
+                <link rel="shortcut icon" href="../imagenes/aeo.ico" />
+                <link href="../css/bootstrap.min.css" rel="stylesheet">
+                <link href="../css/estilos.css" rel="stylesheet">
+            </head>
+            <body>
+        <!fin documento inicio>
+
+        <!Barra de navegacion Navbar>
+        <script type="text/javascript" src="../js/jquery-2.2.4.min.js"></script>
+        <head><link href="../css/estilos_melvin.css" rel="stylesheet"></head>
+        <nav class="navbar navbar-default ">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a  href="../index.php"><img class="btn-card" src="../imagenes/aeo.png" align="left" height="50"></a><!--Para ponerle una img ala pagina -->
+                    <a class="navbar-brand" href="../index.php"><strong>Agenda Electrónica Oriental</strong></a>
+                </div>
+
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li> <a id="colorIniciosecion" href="../../config/cerrarSessionLogin.php"><span class="glyphicon glyphicon-off" aria-hidden="true"></span> <strong>Cerrar Sesión</strong></a></li>
+
+                        <li id="boton" class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> <strong>Panel de Control</strong><span class="caret"></span></a>
+                            <ul id="despliege"class="dropdown-menu" role="menu">
+                                <li><a href="../Vistas/mostrar_usuarios.php"><img src="../imagenes/administracioncuenta.jpg" height="15"></img> <strong>Administración de Cuenta</strong></a></li>
+                                <li><a href="../Vistas/administracion-de-perfiles.php"><img src="../imagenes/administracionperfil.jpg" height="15"></img> <strong>Administración de Perfil</strong></a></li>
+                            </ul>
+                        </li>
+                        <!--va iniciar secion o registrarce -->
+                    </ul>
+                </div>
+            </div>
+
+        </nav>
+        <!Fin de navbar>
+
         <head>
             <link href="../css/nuevoperfil.css" rel="stylesheet">
             <link href="../css/estilos_melvin.css" rel="stylesheet">
@@ -16,7 +72,7 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
                     <div class="panel panel-default">
                         <div class="panel-heading " style="height: 40px">
 
-                            <h3 class="panel-title"><span class="glyphicon glyphicon-pencil"></span>   Edición de Perfil</h3>     
+                            <h3 class="panel-title"><span class="glyphicon glyphicon-pencil"></span>   Edición de Perfil</h3>
                         </div>
 
                         <div class="panel-body">
@@ -26,7 +82,7 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
                                 </div><br><br>
 
 
-                                <div class="group" id="nombreDeOrg">   
+                                <div class="group" id="nombreDeOrg">
                                     <input  type="text" required name="nomborg_rec" id="nombreOrg">
                                     <span id="lbNomb" class="highlight"></span>
                                     <span class="bar"></span>
@@ -50,7 +106,7 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
                                 </div>
 
 
-                                <div class="group">      
+                                <div class="group">
                                     <input class="input" type="text" required id="dirOrg" name="direccion_rec">
                                     <span class="highlight"></span>
                                     <span class="bar"></span>
@@ -67,7 +123,7 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
 
 
 
-                                <div class="group">    
+                                <div class="group">
                                     <input class="input" type="text" required id="descOrg" name="desc_rec">
                                     <span class="highlight"></span>
                                     <span class="bar"></span>
@@ -76,7 +132,7 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
 
 
 
-                                <div class="group">     
+                                <div class="group">
                                     <input class="input" type="text" id="latOrg" required name="lat_rec" >
                                     <span class="highlight"></span>
                                     <span class="bar"></span>
@@ -84,7 +140,7 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
                                 </div>
 
 
-                                <div class="group">   
+                                <div class="group">
                                     <input class="input" type="text" id="longOrg" required name="longitud_rec">
                                     <span class="highlight"></span>
                                     <span class="bar"></span>
@@ -92,9 +148,9 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
                                 </div>
                                 <h5>Ingrese su Ubicación</h5>
 
-                                <script type="text/javascript" 
+                                <script type="text/javascript"
                                         src="https://maps.google.com/maps/api/js?sensor=false">
-                                </script> 
+                                </script>
 
                                 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjOpSe_s3D6bX5abrOcQ5Yg8GGmUdhQn8&callback=initMap"
                                 type="text/javascript"></script>
@@ -134,9 +190,9 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
 
                                             }
 
-                                </script> 
+                                </script>
                                 <body onload="initialize()">
-                                    <div id="map_canvas" style="width:100%; height:200px"></div><br> 
+                                    <div id="map_canvas" style="width:100%; height:200px"></div><br>
 
                                 </body
 
@@ -201,7 +257,7 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
 
                         </div>
                     </div>
-                </div>  
+                </div>
             </div>
         </div>
         <br>
@@ -468,10 +524,46 @@ if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
 
 
         </script>
+<!inicio de documento cierre>
+        <script src="../js/jquery.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+        <br>
+        <br>
 
-        <?php
-        include_once '../plantillas/documento-cierre.inc.php';
-        ?>
+        <!-- Footer -->
+        <div class="navbar navbar-default navbar-fixed-bottom" id="footer">
+            <div class="container">
+                <div class="row text-center text-xs-center text-sm-left text-md-left ">
+                    <div  class="col-xs-6 col-sm-6 col-md-4" >
+                        <a  href="../Vistas/acercadeweb.php"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> <strong class="opcion coloremail">Acerca de Desarrolladores</strong></a>
+                    </div>
+
+                    <div  class="col-xs-6 col-sm-6 col-md-4" >
+                        <a  href="#"><span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> <strong class="opcion coloremail">Descarga la App</strong></a>
+
+                        <ul class="list-unstyled quick-links">
+
+                            <div class="principal"
+                        </div>
+
+                        <!--LIK DE DESCARGA DE LA APP-->
+                    </ul>
+                </div>
+                <div  class="col-xs-6 col-sm-6 col-md-4" >
+                    <span  class="glyphicon glyphicon-question-sign" aria-hidden="true"></span> <strong id="coloayuda">Ayuda y Contacto</strong></a>
+
+                    <ul class="list-unstyled quick-links">
+                        <a  href="#"><span  aria-hidden="true"></span> <strong class="opcion coloremail"><small>aeodanli@gmail.com</small></strong></a>
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </div>
+        </body>
+        </html>
+      <!fin de cierre>
         <?php
     } else {
         header('Location: /webaeo');
