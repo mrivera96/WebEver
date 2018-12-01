767bf406969945718d75ee92492d020693bbb165
bessyRamos <bessyramos3@gmail.com>
2018-12-01 14:02:16 -0600

    modificacion de las categorias y regiones del buscador.

--- src/vistas/buscador.inc.php
+++ src/vistas/buscador.inc.php
@@ -1,3 +1,4 @@
+
 <?php
 $busquedas = null;
 ?>
@@ -17,27 +18,15 @@ $busquedas = null;
             </div>
           </div>
           <div class="col-md-3 col-xs-3 col-sm-3 inner-addon right-addon">
-            <select class="form-control" name="categoria">
-              <option value="0">Todas las Categorías</option>
-              <option value="1" >Emergencia</option>
-              <option value="2" >Educación</option>
-              <option value="3">Centros Asistenciales</option>
-              <option value="4">Bancos</option>
-              <option value="5" >Hostelería y turismo</option>
-              <option value="6" >Instituciones Públicas</option>
-              <option value="7">Comercio de Bienes</option>
-              <option value="8" >Comercio de Servicios</option>
-              <option value="9">Bienes y Raíces</option>
-              <option value="10">Asesoría Legal</option>
-              <option value="11">Funeraria</option>
-            </select>
+           <select class="form-control" id="categoria" name="categoria">
+             <option value="" >Todas La Categorias</option>
+
+           </select>
           </div>
           <div class="col-md-3 col-xs-3 col-sm-3 inner-addon right-addon">
-            <select class="form-control" name="region">
-              <option value="0" >Todas las Regiones</option>
-              <option value="4" >El Paraíso</option>
-              <option value="3">Danlí</option>
-            </select>
+             <select class="form-control" id="region" name="region">
+               <option value="" >Todas Las Regiones</option>
+                </select>
           </div>
         </div>
       </div>
